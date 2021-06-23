5573e9507f4a4459ed260180b342a93029d7ec99
annasemkaobg <annasemkaobg>
2021-06-23 17:50:59 +0300

    E2E Create Project

--- cypress/integration/admin/CreateProject/createproject.js
+++ cypress/integration/admin/CreateProject/createproject.js
@@ -9,7 +9,7 @@ describe("My Admin CreateProject Test Suite", function () {
   });
 
   it("1. Create ContentModeration Project", function () {
-    cy.get('[class="sc-giIncl efCwnz"]').click();
+    cy.get('[data-testid="add-project-button"]').click();
     cy.get('[class="ant-select-selection-search-input"]').click();
     cy.get('[class="ant-select-item-option-content"]')
       .contains("My best company", { matchCase: false })
@@ -20,17 +20,44 @@ describe("My Admin CreateProject Test Suite", function () {
         matchCase: false,
       })
       .click();
-    cy.fixture("2pic.zip").then((fileContent) => {
+    cy.fixture("2pic.zip")
+      .as("FileUpload")
+      .then((fileContent) => {
+        cy.get('input[type="file"]').attachFile({
+          fileContent: fileContent.toString(),
+          fileName: "2pic.zip",
+          mimeType: "application/zip",
+        });
+      });
+    cy.wait(4000);
+    cy.get("[data-testid=create-project_button_step-next]").click();
+    cy.get('input[name="labels"]').type("test{enter}");
+    cy.get("[data-testid=create-project_button_step-next]").click();
+    cy.fixture("instructions.pdf").then((fileContent) => {
       cy.get('input[type="file"]').attachFile({
         fileContent: fileContent.toString(),
-        fileName: "2pic.zip",
+        fileName: "instructions.pdf",
+        mimeType: "application/pdf",
       });
     });
+    cy.wait(2000);
+    cy.get('textarea[name="adminShortInstruction"]').type(
+      "Need to be done asap!"
+    );
     cy.get("[data-testid=create-project_button_step-next]").click();
+    cy.get("[id=helperRate]").type("1");
+    cy.get("[id=auditorRate]").type("1");
+    cy.get("[data-testid=create-project_button_step-next]").click();
+    cy.contains(
+      "CONGRATULATIONS, YOU'RE ALL DONE! We will review your request and will get back to you shortly!"
+    ).should("be.visible");
+    cy.get(
+      "[data-testid=create-project_button_step-next_save-without-helpers]"
+    ).click();
   });
 
   it("2. Create ImageAnnotation Project", function () {
-    cy.get('[class="sc-giIncl efCwnz"]').click();
+    cy.get('[data-testid="add-project-button"]').click();
     cy.get('[class="ant-select-selection-search-input"]').click();
     cy.get('[class="ant-select-item-option-content"]')
       .contains("My best company", { matchCase: false })
@@ -45,13 +72,39 @@ describe("My Admin CreateProject Test Suite", function () {
       cy.get('input[type="file"]').attachFile({
         fileContent: fileContent.toString(),
         fileName: "2pic.zip",
+        mimeType: "application/zip",
       });
     });
+    cy.wait(5000);
     cy.get("[data-testid=create-project_button_step-next]").click();
+    cy.get('input[name="customLabels"]').check().should("be.checked");
+    cy.get('input[name="labels"]').type("test{enter}");
+    cy.get("[data-testid=create-project_button_step-next]").click();
+    cy.fixture("instructions.pdf").then((fileContent) => {
+      cy.get('input[type="file"]').attachFile({
+        fileContent: fileContent.toString(),
+        fileName: "instructions.pdf",
+        mimeType: "application/pdf",
+      });
+    });
+    cy.wait(2000);
+    cy.get('textarea[name="adminShortInstruction"]').type(
+      "Need to be done asap!"
+    );
+    cy.get("[data-testid=create-project_button_step-next]").click();
+    cy.get("[id=helperRate]").type("1");
+    cy.get("[id=auditorRate]").type("1");
+    cy.get("[data-testid=create-project_button_step-next]").click();
+    cy.contains(
+      "CONGRATULATIONS, YOU'RE ALL DONE! We will review your request and will get back to you shortly!"
+    ).should("be.visible");
+    cy.get(
+      "[data-testid=create-project_button_step-next_save-without-helpers]"
+    ).click();
   });
 
   it("3. Create Digitization Project", function () {
-    cy.get('[class="sc-giIncl efCwnz"]').click();
+    cy.get('[data-testid="add-project-button"]').click();
     cy.get('[class="ant-select-selection-search-input"]').click();
     cy.get('[class="ant-select-item-option-content"]')
       .contains("My best company", { matchCase: false })
@@ -68,6 +121,30 @@ describe("My Admin CreateProject Test Suite", function () {
         fileName: "2pic.zip",
       });
     });
+    cy.wait(4000);
     cy.get("[data-testid=create-project_button_step-next]").click();
+    cy.get('input[name="customLabels"]').check().should("be.checked");
+    cy.get('input[name="labels"]').type("test{enter}");
+    cy.get("[data-testid=create-project_button_step-next]").click();
+    cy.fixture("instructions.pdf").then((fileContent) => {
+      cy.get('input[type="file"]').attachFile({
+        fileContent: fileContent.toString(),
+        fileName: "instructions.pdf",
+      });
+    });
+    cy.get('textarea[name="adminShortInstruction"]').type(
+      "Need to be done asap!"
+    );
+    cy.wait(2000);
+    cy.get("[data-testid=create-project_button_step-next]").click();
+    cy.get("[id=helperRate]").type("1");
+    cy.get("[id=auditorRate]").type("1");
+    cy.get("[data-testid=create-project_button_step-next]").click();
+    cy.contains(
+      "CONGRATULATIONS, YOU'RE ALL DONE! We will review your request and will get back to you shortly!"
+    ).should("be.visible");
+    cy.get(
+      "[data-testid=create-project_button_step-next_save-without-helpers]"
+    ).click();
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,5 @@
 import "cypress-file-upload";
-
+import "cypress-wait-until";
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
